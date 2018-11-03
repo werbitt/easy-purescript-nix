a34d00cf62a25cc29d76c1f1cebabcf75f959a7f
Micah Werbitt <micah@werbitt.net>
2018-11-03 08:59:13 -0400

    Update purs.nix to work with macOS

--- purs.nix
+++ purs.nix
@@ -3,9 +3,12 @@
 pkgs.stdenv.mkDerivation rec {
   name = "purs-simple";
   version = "v0.12.0";
+  platform = if pkgs.stdenv.targetPlatform.isMacOS
+             then "macos"
+             else "linux64";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/purescript/purescript/releases/download/${version}/linux64.tar.gz";
+    url = "https://github.com/purescript/purescript/releases/download/${version}/${platform}.tar.gz";
     sha256 = "1wf7n5y8qsa0s2p0nb5q81ck6ajfyp9ijr72bf6j6bhc6pcpgmyc";
   };
 
@@ -13,4 +16,4 @@ pkgs.stdenv.mkDerivation rec {
     mkdir -p $out/bin
     install -D -m555 -T purs $out/bin/purs
   '';
-}
+  }
