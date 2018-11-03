2c9fbdb0120370f7c1fe27ee95b0c5c866f20cf3
Micah Werbitt <micah@werbitt.net>
2018-11-03 09:35:27 -0400

    Fix purs.nix

--- purs.nix
+++ purs.nix
@@ -1,19 +1,31 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  version = "v0.12.0";
+  tgt = {
+          "x86_64-darwin" =
+            { url = "https://github.com/purescript/purescript/releases/download/${version}/macos.tar.gz";
+              sha256 = "0fnkj0b8ll159svz7gxsi0ph7dnf6ay5p14jikd983f7m65z5jfb";
+            };
+
+          "x86_64-linux" =
+            { url = "https://github.com/purescript/purescript/releases/download/${version}/linux.tar.gz";
+              sha256 = "1wf7n5y8qsa0s2p0nb5q81ck6ajfyp9ijr72bf6j6bhc6pcpgmyc";
+            };
+        }.${pkgs.stdenv.system};
+
+in
 pkgs.stdenv.mkDerivation rec {
   name = "purs-simple";
-  version = "v0.12.0";
-  platform = if pkgs.stdenv.targetPlatform.isMacOS
-             then "macos"
-             else "linux64";
+
 
   src = pkgs.fetchurl {
-    url = "https://github.com/purescript/purescript/releases/download/${version}/${platform}.tar.gz";
-    sha256 = "1wf7n5y8qsa0s2p0nb5q81ck6ajfyp9ijr72bf6j6bhc6pcpgmyc";
+    url = tgt.url;
+    sha256 = tgt.sha256;
   };
 
   installPhase = ''
     mkdir -p $out/bin
-    install -D -m555 -T purs $out/bin/purs
-  '';
+      install -D -m555 -T purs $out/bin/purs
+    '';
   }
